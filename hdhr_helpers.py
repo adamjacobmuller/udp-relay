68e0f7016ed5d7ee3edf87039fcba8d26f1f189d
Adam Jacob Muller <adam@isprime.com>
2016-01-17 23:35:35 -0500

    pep8

--- hdhr_helpers.py
+++ hdhr_helpers.py
@@ -1,22 +1,24 @@
-def get_vstatus_packed(libhdhr,device):
-    vstatus=libhdhr.device_get_tuner_vstatus(device)
+def get_vstatus_packed(libhdhr, device):
+    vstatus = libhdhr.device_get_tuner_vstatus(device)
     return vstatus_pack(vstatus)
 
+
 def vstatus_pack(vstatus):
-    if vstatus[2].not_subscribed==1:
-        subscribed='f'
+    if vstatus[2].not_subscribed == 1:
+        subscribed = 'f'
     else:
-        subscribed='t'
+        subscribed = 't'
 
-    if vstatus[2].not_available==1:
-        available='f'
+    if vstatus[2].not_available == 1:
+        available = 'f'
     else:
-        available='t'
+        available = 't'
 
-    if vstatus[2].copy_protected==1:
-        copy_protected='t'
+    if vstatus[2].copy_protected == 1:
+        copy_protected = 't'
     else:
-        copy_protected='f'
+        copy_protected = 'f'
+
     return 'vchannel=%s:name=%s:auth=%s:cci=%s:cgms=%s:subscribed=%s:avaliable=%s:copy_protected=%s' % (
         vstatus[2].vchannel,
         vstatus[2].name,
@@ -26,4 +28,4 @@ def vstatus_pack(vstatus):
         subscribed,
         available,
         copy_protected
-        )
+    )
